6fd699a2aa8897eef31c283d916a7536352a0412
Robert Brice <rob.a.brice@gmail.comC>
2017-02-28 15:05:16 +0000

    added view all expenditure items by category

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -3,3 +3,9 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 
 require('./models/category.rb')
+
+get '/categories/:id' do
+@category = Category.return_by_id(params[:id])
+@transactions = Transaction.all
+erb(:"/categories/index")
+end
