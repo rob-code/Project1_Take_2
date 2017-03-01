5a063e177be03155850402a127bce32186cbe596
Robert Brice <rob.a.brice@gmail.comC>
2017-03-01 14:58:28 +0000

    add create new spending category

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -4,6 +4,20 @@ require('pry-byebug')
 
 require('./models/category.rb')
 
+
+
+get '/categories/new' do
+@categories = Category.all
+erb(:'categories/new')
+end
+
+post '/categories' do
+@category = Category.new(params)
+@category.save
+redirect to("dashboard")
+end
+
+
 get '/categories/:id' do
 @category = Category.return_by_id(params[:id])
 @transactions = Transaction.all
